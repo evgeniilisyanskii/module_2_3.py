2bcf10ebad95913acf92ab1bb27714e4bf57b282
evgeniilisyanskii <evgeniilisyanskii@yandex.ru>
2024-08-29 00:01:53 +1000

    Add files via upload

---
+++ module_2_3.pу.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+positive_numbers = 0
+while  positive_numbers < len(my_list):
+    if my_list[positive_numbers] > 0:
+        print(my_list[positive_numbers])
+        positive_numbers += 1
+    if my_list[positive_numbers] < 0:
+        break
+
+
+
+
