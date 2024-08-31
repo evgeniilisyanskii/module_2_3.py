ef0f1f7014aaefc0687cc7c42ec23c7d9921b54a
evgeniilisyanskii <evgeniilisyanskii@yandex.ru>
2024-08-31 11:26:02 +1000

    Update module_2_3.pу.py

--- module_2_3.pу.py
+++ module_2_3.pу.py
@@ -1,12 +1,16 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-my_list.remove (0)
 positive_numbers = 0
 while  positive_numbers < len(my_list):
-    if my_list[positive_numbers] > 0:
-        print(my_list[positive_numbers])
+    if my_list[positive_numbers] == 0:
+        positive_numbers += 1
+        continue
         positive_numbers += 1
     if my_list[positive_numbers] < 0:
         break
+    print(my_list[positive_numbers])
+    positive_numbers += 1
+
+
 
 
 
